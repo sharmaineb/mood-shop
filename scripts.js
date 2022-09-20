ca40b869e2e2e78301224dd25f248d2c11f3c7f8
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-09-20 14:00:12 -0700

    displayed moods with button using javascript

---
+++ scripts.js
@@ -0,0 +1,42 @@
+import data from './data.js'
+
+const itemsContainer = document.querySelector('#items')
+const itemList = document.getElementById("item-list")
+const cartQty = document.getElementById("cart-qty")
+const cartTotal = document.getElementById("cart-total")
+
+for (let i = 0; i < data.length; i += 1) {
+    // create a new div element and give it a class name
+    const newDiv = document.createElement('div');
+    newDiv.className = 'item'
+    // create an image element
+    const img = document.createElement('img');
+    // this will change each time we go through the loop. can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+    // Add the image to the div
+    newDiv.appendChild(img)
+    console.log(img)
+    itemsContainer.appendChild(newDiv)
+    // Create a paragraph element for a description
+    const desc = document.createElement("P")
+    // Give the paragraph text from the data
+    desc.innerText = data[i].desc
+    // append the paragraph to the div
+    newDiv.appendChild(desc)
+    // do the  same thing for price
+    const price = document.createElement("P")
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+    // Make a button
+    const button = document.createElement("button")
+    // Add an id name to the button
+    button.id = data[i].name
+    // creates a custom attribute called data-price. That will hold price for each element in the button
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+
+}
+
