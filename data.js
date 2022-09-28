d0e514280232529ae566764fdb8601c326df6bfb
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-09-27 22:34:22 -0700

    styled some more

--- data.js
+++ data.js
@@ -2,73 +2,73 @@ const data = [{
   "id": 1,
   "name": "happy",
   "image": "images/bts.gif",
-  "desc": "Happiness lies in the joy of achievement and the thrill of creative effort.",
+  "desc": "Be happy. It really annoys negative people",
   "price": 5.99
 }, {
   "id": 2,
   "name": 'sad',
   "image": 'images/b992.gif',
-  "desc": "Tears are words that can’t be explained.",
+  "desc": "One day I'm going to make onions cry.",
   "price": 5.99
 }, {
   "id": 3,
   "name": 'angry',
-  "image": "images/raya.gif",
-  "desc": "Anger is like a storm rising up from the bottom of your consciousness. When you feel it coming, turn your focus to your breath.",
+  "image": "images/b993.gif",
+  "desc": "Coffee, because anger management is expensive.",
   "price": 5.99
 }, {
   "id": 4,
   "name": 'calm',
-  "image": "images/sc1.gif",
-  "desc": "To be calm is the highest achievement of the self.",
+  "image": "images/community.gif",
+  "desc": "Never in the history of calming down has anyone calmed down by being told to calm down.",
   "price": 5.99
 }, {
   "id": 5,
   "name": 'curious',
   "image": "images/gg.gif",
-  "desc": "I have no special talents. I am only passionately curious.",
+  "desc": "Millions saw the apple fall, Newton was the only one who asked why?",
   "price": 5.99
 }, {
   "id": 6,
   "name": 'disgust',
   "image": "images/io.gif",
-  "desc": "Not all disgust need produce symptoms of nausea, nor all nausea mark the presence of disgust.",
+  "desc": "Joy would know what to do.",
   "price": 5.99
 }, {
   "id": 7,
   "name": 'energetic',
   "image": "images/tdwp.gif",
-  "desc": "Be the energy you want to attract.",
+  "desc": "I'll get there. I'm on low-power mode right now.",
   "price": 5.99
 }, {
   "id": 8,
   "name": 'gloomy',
   "image": "images/abbott3.gif",
-  "desc": "All was gloomy and awful. There was no light at the end of the tunnel - or if there was, it was an oncoming train.",
+  "desc": "Adulthood is like looking both ways before crossing the street and then getting hit by an airplane.",
   "price": 5.99
 }, {
   "id": 9,
   "name": 'scared',
   "image": "images/st1.gif",
-  "desc": "The only thing we have to fear is fear itself.",
+  "desc": "You have never experienced true fear until a poster falls off the wall in the middle of the night.",
   "price": 5.99
 }, {
   "id": 10,
   "name": 'shy',
   "image": "images/ce.gif",
-  "desc": "When a shy person smiles, it's like the sun coming out.",
+  "desc": "I'm shy at first. But I do the goofiest randomest things when I get comfortable with someone.",
   "price": 5.99
 }, {
   "id": 11,
   "name": 'sleepy',
   "image": "images/disney.gif",
-  "desc": "Your future depends on your dreams, so go to sleep.",
+  "desc": "Me risking my job, career, and future to get an extra 15 minutes of sleep.",
   "price": 5.99
 }, {
   "id": 12,
   "name": 'surprised',
-  "image": "images/abbott2.gif",
-  "desc": "The only thing that should surprise us is that there are still some things that can surprise us.",
+  "image": "images/sc3.gif",
+  "desc": "When I was born, I was so surprised I didn't talk for a year.",
   "price": 5.99
 }]
 
